96ae6f4b195e11dde5fa3c93fb1f48a186d63c90
jiink <jacob.j.carlson03@gmail.com>
2024-04-15 12:17:29 -0700

    Add block and blockitem

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,15 +1,22 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final Block WOODEN_FURNACE = new Block(Block.Settings.create().strength(1.0f));
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +25,7 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.BLOCK, new Identifier("smelting-in-a-pinch", "wooden_furnace"), WOODEN_FURNACE);
+		Registry.register(Registries.ITEM, new Identifier("smelting-in-a-pinch", "wooden_furnace"), new BlockItem(WOODEN_FURNACE, new Item.Settings()));
 	}
 }
