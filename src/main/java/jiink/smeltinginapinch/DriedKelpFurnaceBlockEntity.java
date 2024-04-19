cb614d85a42d97029f6455744e4866e0c6aaba3d
jiink <jacob.j.carlson03@gmail.com>
2024-04-19 11:35:52 -0700

    Add blaze, charcoal, dried kelp, lava, and gunpowder furnaces

---
+++ src/main/java/jiink/smeltinginapinch/DriedKelpFurnaceBlockEntity.java
@@ -0,0 +1,34 @@
+package jiink.smeltinginapinch;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+
+public class DriedKelpFurnaceBlockEntity extends DisposableFurnaceBlockEntity {
+    public DriedKelpFurnaceBlockEntity(BlockPos pos, BlockState state) {
+        super(pos, state, 8, 8.0F, SmeltingInAPinch.DRIED_KELP_FURNACE_BLOCK_ENTITY);
+    }
+
+    @Override
+    protected void burnoutDestroy(World world, BlockPos pos, BlockState state) {
+        // explode
+        //world.createExplosion(null, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 4.0f, true, ExplosionSourceType.MOB);
+        // make block breaking particles and sound
+        world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, pos, Block.getRawIdFromState(state));
+        world.playSound(
+                null,
+                pos,
+                SoundEvents.BLOCK_SCAFFOLDING_BREAK,
+                SoundCategory.BLOCKS,
+                1f,
+                0.75f
+        );
+        // destroy block and replace it with fire
+        world.setBlockState(pos, net.minecraft.block.Blocks.FIRE.getDefaultState());
+    }
+}
--- src/main/java/jiink/smeltinginapinch/SmeltingInAPinch.java
+++ src/main/java/jiink/smeltinginapinch/SmeltingInAPinch.java
@@ -64,22 +64,24 @@ public class SmeltingInAPinch implements ModInitializer {
 			new Identifier(MOD_ID, "wooden_furnace_block_entity"),
 			FabricBlockEntityTypeBuilder.create(WoodenFurnaceBlockEntity::new, WOODEN_FURNACE_BLOCK).build()
 	);
+
 	// Dried kelp furnace stuff
 	public static final Block DRIED_KELP_FURNACE_BLOCK = Registry.register(
 			Registries.BLOCK,
 			new Identifier(MOD_ID, "dried_kelp_furnace"),
-			new CoalFurnaceBlock(FabricBlockSettings.create().strength(5.0f).sounds(BlockSoundGroup.STONE).burnable().luminance(blockstateToLuminance()))
+			new DriedKelpFurnaceBlock(FabricBlockSettings.create().strength(5.0f).sounds(BlockSoundGroup.STONE).burnable().luminance(blockstateToLuminance()))
 	);
 	public static final Item DRIED_KELP_FURANCE_BLOCK_ITEM = Registry.register(
 			Registries.ITEM,
 			new Identifier(MOD_ID, "dried_kelp_furnace"),
 			new BlockItem(DRIED_KELP_FURNACE_BLOCK, new Item.Settings())
 	);
-	public static final BlockEntityType<CoalFurnaceBlockEntity> DRIED_KELP_FURNACE_BLOCK_ENTITY = Registry.register(
+	public static final BlockEntityType<DriedKelpFurnaceBlockEntity> DRIED_KELP_FURNACE_BLOCK_ENTITY = Registry.register(
 			Registries.BLOCK_ENTITY_TYPE,
 			new Identifier(MOD_ID, "dried_kelp_furnace_block_entity"),
-			FabricBlockEntityTypeBuilder.create(CoalFurnaceBlockEntity::new, DRIED_KELP_FURNACE_BLOCK).build()
+			FabricBlockEntityTypeBuilder.create(DriedKelpFurnaceBlockEntity::new, DRIED_KELP_FURNACE_BLOCK).build()
 	);
+
 	// Coal furnace stuff
 	public static final Block COAL_FURNACE_BLOCK = Registry.register(
 			Registries.BLOCK,
@@ -96,6 +98,24 @@ public class SmeltingInAPinch implements ModInitializer {
 			new Identifier(MOD_ID, "coal_furnace_block_entity"),
 			FabricBlockEntityTypeBuilder.create(CoalFurnaceBlockEntity::new, COAL_FURNACE_BLOCK).build()
 	);
+
+	// Charcoal furnace stuff
+	public static final Block CHARCOAL_FURNACE_BLOCK = Registry.register(
+			Registries.BLOCK,
+			new Identifier(MOD_ID, "charcoal_furnace"),
+			new CharcoalFurnaceBlock(FabricBlockSettings.create().strength(5.0f).sounds(BlockSoundGroup.STONE).burnable().luminance(blockstateToLuminance()))
+	);
+	public static final Item CHARCOAL_FURANCE_BLOCK_ITEM = Registry.register(
+			Registries.ITEM,
+			new Identifier(MOD_ID, "charcoal_furnace"),
+			new BlockItem(CHARCOAL_FURNACE_BLOCK, new Item.Settings())
+	);
+	public static final BlockEntityType<CharcoalFurnaceBlockEntity> CHARCOAL_FURNACE_BLOCK_ENTITY = Registry.register(
+			Registries.BLOCK_ENTITY_TYPE,
+			new Identifier(MOD_ID, "charcoal_furnace_block_entity"),
+			FabricBlockEntityTypeBuilder.create(CharcoalFurnaceBlockEntity::new, CHARCOAL_FURNACE_BLOCK).build()
+	);
+
 	// Blaze furnace stuff
 	public static final Block BLAZE_FURNACE_BLOCK = Registry.register(
 			Registries.BLOCK,
@@ -113,6 +133,40 @@ public class SmeltingInAPinch implements ModInitializer {
 			FabricBlockEntityTypeBuilder.create(BlazeFurnaceBlockEntity::new, BLAZE_FURNACE_BLOCK).build()
 	);
 
+	// Lava furnace stuff
+	public static final Block LAVA_FURNACE_BLOCK = Registry.register(
+			Registries.BLOCK,
+			new Identifier(MOD_ID, "lava_furnace"),
+			new LavaFurnaceBlock(FabricBlockSettings.create().strength(5.0f).sounds(BlockSoundGroup.STONE).burnable().luminance(blockstateToLuminance()))
+	);
+	public static final Item LAVA_FURANCE_BLOCK_ITEM = Registry.register(
+			Registries.ITEM,
+			new Identifier(MOD_ID, "lava_furnace"),
+			new BlockItem(LAVA_FURNACE_BLOCK, new Item.Settings())
+	);
+	public static final BlockEntityType<LavaFurnaceBlockEntity> LAVA_FURNACE_BLOCK_ENTITY = Registry.register(
+			Registries.BLOCK_ENTITY_TYPE,
+			new Identifier(MOD_ID, "lava_furnace_block_entity"),
+			FabricBlockEntityTypeBuilder.create(LavaFurnaceBlockEntity::new, LAVA_FURNACE_BLOCK).build()
+	);
+
+	// Gunpowder furnace stuff
+	public static final Block GUNPOWDER_FURNACE_BLOCK = Registry.register(
+			Registries.BLOCK,
+			new Identifier(MOD_ID, "gunpowder_furnace"),
+			new GunpowderFurnaceBlock(FabricBlockSettings.create().strength(5.0f).sounds(BlockSoundGroup.STONE).burnable().luminance(blockstateToLuminance()))
+	);
+	public static final Item GUNPOWDER_FURANCE_BLOCK_ITEM = Registry.register(
+			Registries.ITEM,
+			new Identifier(MOD_ID, "gunpowder_furnace"),
+			new BlockItem(GUNPOWDER_FURNACE_BLOCK, new Item.Settings())
+	);
+	public static final BlockEntityType<GunpowderFurnaceBlockEntity> GUNPOWDER_FURNACE_BLOCK_ENTITY = Registry.register(
+			Registries.BLOCK_ENTITY_TYPE,
+			new Identifier(MOD_ID, "gunpowder_furnace_block_entity"),
+			FabricBlockEntityTypeBuilder.create(GunpowderFurnaceBlockEntity::new, GUNPOWDER_FURNACE_BLOCK).build()
+	);
+
 	public static final ScreenHandlerType<DisposableFurnaceScreenHandler> DISPOSABLE_FURNACE_SCREEN_HANDLER;
 	static	{
 		DISPOSABLE_FURNACE_SCREEN_HANDLER = Registry.register(
@@ -128,18 +182,30 @@ public class SmeltingInAPinch implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Getting ready to smelt!");
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+			content.addAfter(Items.BLAST_FURNACE, GUNPOWDER_FURANCE_BLOCK_ITEM);
+			content.addAfter(Items.BLAST_FURNACE, LAVA_FURANCE_BLOCK_ITEM);
+			content.addAfter(Items.BLAST_FURNACE, BLAZE_FURANCE_BLOCK_ITEM);
+			content.addAfter(Items.BLAST_FURNACE, CHARCOAL_FURANCE_BLOCK_ITEM);
 			content.addAfter(Items.BLAST_FURNACE, COAL_FURANCE_BLOCK_ITEM);
+			content.addAfter(Items.BLAST_FURNACE, DRIED_KELP_FURANCE_BLOCK_ITEM);
 			content.addAfter(Items.BLAST_FURNACE, WOODEN_FURANCE_BLOCK_ITEM);
 			content.addAfter(Items.TORCH, DEMO_BLOCK_ITEM);
 		});
 
 		FlammableBlockRegistry.getDefaultInstance().add(WOODEN_FURNACE_BLOCK, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(DRIED_KELP_FURNACE_BLOCK, 5, 5);
 		FlammableBlockRegistry.getDefaultInstance().add(COAL_FURNACE_BLOCK, 5, 5);
+		FlammableBlockRegistry.getDefaultInstance().add(CHARCOAL_FURNACE_BLOCK, 5, 5);
+		// TODO: Make lava and blaze furnaces burn other stuff around it like real lava does.
 
 		FuelRegistry.INSTANCE.add(WOODEN_FURANCE_BLOCK_ITEM, 300 * 8);
+		FuelRegistry.INSTANCE.add(DRIED_KELP_FURANCE_BLOCK_ITEM, 1600 * 8); // TODO
 		FuelRegistry.INSTANCE.add(COAL_FURANCE_BLOCK_ITEM, 1600 * 8);
+		FuelRegistry.INSTANCE.add(CHARCOAL_FURANCE_BLOCK_ITEM, 1600 * 8);
+		FuelRegistry.INSTANCE.add(LAVA_FURANCE_BLOCK_ITEM, 1600 * 8);
+		FuelRegistry.INSTANCE.add(BLAZE_FURANCE_BLOCK_ITEM, 1600 * 8);
 	}
 
 	private static ToIntFunction<BlockState> blockstateToLuminance() {
